1bb1426a1b987e8567e7445486f59242148966b3
Ibrokhim702 <rakhimovaziz077@gmail.com>
2024-12-18 19:39:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+
+// // const answer = +prompt("moshina tanlang")
+// // const cars = +prompt["BMW","KIA","MERS","AUDI",]
+
+// // if(cars == 0){
+// //     alert("Siz BMW kiritingiz")
+// // }
+// // if(cars == 1){
+// //     alert("Siz KIA kiritingiz")
+// // }
+// // if(cars == 2){
+// //     alert("Siz MERS kiritingiz")
+// // }
+// // if(cars == 3){
+// //     alert("Siz AUDI  kiritingiz")
+// // }
+
+
+// const userName = prompt("Ism qo'shing")
+// const userr = +prompt("Iltimos tanlang:\n 1) push \n 2) pop \n 3) shift \n 4) unshif")
+// const oquvchilar = ["Doniyor","Tom","Ismoil","Boisxon","Mahmud"];
+
+// console.log(oquvchilar.push(userName));
+// console.log(oquvchilar);
+
+
+// if(userr == 1){
+//     console.log(oquvchilar.push(userName));
+//     console.log(oquvchilar);
+    
+// }
+// if(userr == 2){
+//     console.log(oquvchilar.pop(userName));
+//     console.log(oquvchilar);
+// }
+// if(userr == 3){
+//     console.log(oquvchilar.shift(userName));
+//     console.log(oquvchilar);
+// }
+// if(userr == 4){
+//     console.log(oquvchilar.unshift(userName));
+//     console.log(oquvchilar);    
+// }else{
+//     alert("boshqatdan kirit")
+// }
+
+// const summa = +prompt("Summa kiriting")
+// const user = +prompt("Iltimos kerakli kursni \n 1)Dollar \n 2) Evro \n 3) Rubl")
+
+// const dollarkursi = 12800;
+// const evrokursi = 15250;
+// const rublKursi = 800;
+
+// if(user == 1){
+//     console.log(`${summa} ${summa * dollarkursi} somga teng`);
+// }
+// else if(user == 2){
+//     console.log(`${summa} ${summa * evrokursi} somga teng`);
+// }else if(user == 3){
+//     console.log(`${summa} ${summa * rublKursi} somga teng`);
+// }else{
+//     alert("boshqattan kiriting")
+// }
+
+
+
+// const userName = alert("Boshlanishida Arryning uzunligi:4")
+// const user = confirm("Sizning arryingizdan malumotlarni olib tashlamoqchimiz:")
+// const surname = alert("Boshlanishida Arryning uzunligi:3")
+// const oquvchilar = ["Olma","Banan","Gilos","Shaftoli"];
+
+// console.log(oquvchilar);
+// if()
+
+// const oquvchilar = ["Olma", "Banan", "Gilos", "Shaftoli"];
+// alert("Boshlanishida Arryning uzunligi: " + oquvchilar.length);
+// const user = confirm("Sizning arryingizdan malumotlarni olib tashlamoqchimiz?");
+
+// if (user) {
+//     oquvchilar.pop(); 
+//     alert("Malumot olib tashlandi");
+// } else {
+//     alert("Malumot olib tashlanmadi");
+// }
+// console.log("O'quvchilar ro'yxati:", oquvchilar);
+// alert("Endi Arryning uzunligi: " + oquvchilar.length);
+
+
+
+const oquvchilar = ["banan","shaftoli","olma","Gilos"]
+alert("Boshlanishida Arryning uzunligi:" + oquvchilar.length);
+const user = confirm("Sizning arryingizdan malumotlarni olib tashlamoqchimiz?")
+if(user){
+    oquvchilar.pop();
+    alert("Malumot olib tashlandi")
+}else{
+    alert("Malumot olib tashlanmadi")
+}
+console.log("O'quvchilar ro'yhati", oquvchilar);
+alert("Endi arryning uzunligi:" + oquvchilar.length)
+
+
+
+
